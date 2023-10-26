7a3ec133096bfd70eb26b0100158d20b2d4eeb0e
kwlucas <lucaswkyle@gmail.com>
2023-10-26 12:41:45 -0600

    Created data type file

---
+++ Assets/Scripts/cSharpDataTypes.cs
@@ -0,0 +1,13 @@
+//C sharp is an object orientated programming (OOP) language.
+//Object orientated means that there is a focus on having "Objects" that contain code.
+//C# is a type of OOP that is class based. All that means is that its "Objects" are called a Class
+//So in a C# Script you must enclose your code in a class.
+public class cSharpDataTypes //Public means that this class and its code can be accessed by other scripts
+{
+    //Data types - C# is an implicity typed language all that means is you need to state what kind of data a variable is.
+    private string text = "Sample Text";
+    private bool yesNo = true;
+    private int number = 1;
+    private float decimalNumber = 1.5f;
+    private double decimalNumberWithMoreSpaces = 1.00000000000005d;
+}
