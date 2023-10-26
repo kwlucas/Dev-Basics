d81f1a73571f9c11496485f5049cf574b15782eb
kwlucas <lucaswkyle@gmail.com>
2023-10-26 14:13:11 -0600

    Set up sample behaviours

---
+++ Assets/Scripts/SampleBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//After a class name there is a ":" which means this class is inherited. 
+//Think of it as being a type of something else.
+//This class "SampleBehaviour" is a type of "MonoBehaviour"
+//MonoBehaviour is a Class as well it is just built into Unity for us to use.
+//They can be attatched to objects in the game.
+public class SampleBehavior : MonoBehaviour
+{
+    private int numA;
+    private int numB;
+    private float numC;
+    private string text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log is how you can print things in the Unity console.
+        Debug.Log("The game has started!");
+        numA = 1;
+        numB = 2;
+        numC = 1.5f;
+
+        text = "A + B = " + numA + numB;
+        //TODO: Why does this print the wrong answer? Make it print the correct answer.
+        Debug.Log(text);
+        //TODO: I want to do the same with numbers A and C.
+        // text = "A + C = " + numA + numC;
+        // Debug.Log(text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/cSharpDataTypes.cs
+++ Assets/Scripts/cSharpDataTypes.cs
@@ -1,3 +1,5 @@
+using System;
+
 //C sharp is an object orientated programming (OOP) language.
 //Object orientated means that there is a focus on having "Objects" that contain code.
 //C# is a type of OOP that is class based. All that means is that its "Objects" are called a Class
@@ -14,6 +16,15 @@ public class cSharpDataTypes //Public means that this class and its code can be 
     //Floats and doubles are similar, but basically data of course has "size"
     //Think of the size as the number of characters in something 1.1 has 2 digits. 1.01 has 3 digits.
     //A float is limited to a size (32 bits) and a double has a limit that is double that (64 bits)
-    
+    void fillerMethod()
+    {
+        Console.Write(text);
+        Console.Write(yesNo);
+        Console.Write(number);
+        Console.Write(floatNumber);
+        Console.Write(doubleNumber);
+        
+    }
+
 
 }
